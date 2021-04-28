d3224a35acb68fa53b7d656bb5388a3503d6ab96
Vicky <43vicky@live.com>
2021-04-28 18:26:59 +0800

    Complete DFA command-line program.

---
+++ DFA.py
@@ -0,0 +1,48 @@
+class DFA:
+
+    def __init__(self, states, alphabets, transition_dict, start_state, accept_states):
+        """Initialize DFA object"""
+        self.STATES = states
+        self.SIGMA = alphabets
+        self.DELTA = transition_dict
+        self.START_STATE = start_state
+        self.ACCEPT_STATES = accept_states
+        self.CURRENT_STATE = None
+
+    def run_state_transition(self, input_symbol):
+        """Takes in current state and goes to next state based on input symbol."""
+        if self.CURRENT_STATE == 'REJECT':
+            return False
+
+        current_state_dict = self.DELTA.get(self.CURRENT_STATE, "TRAP")
+        if current_state_dict != "TRAP":
+            next_state = current_state_dict.get(input_symbol, "TRAP")
+            print("CURRENT STATE : {}\tINPUT SYMBOL : {}\t NEXT STATE : {}".format(self.CURRENT_STATE, input_symbol,
+                                                                                   next_state))
+            if next_state != "TRAP":
+                self.CURRENT_STATE = next_state
+            else:
+                # enter trap state
+                self.CURRENT_STATE = "REJECT"
+
+        else:
+            # enter trap state
+            self.CURRENT_STATE = "REJECT"
+
+    def check_if_accept(self):
+        """Checks if the current state is one of the accept states."""
+        if self.CURRENT_STATE in self.ACCEPT_STATES:
+            return True
+        else:
+            return False
+
+    def run_machine(self, in_string):
+        """Run the machine on input string"""
+        self.CURRENT_STATE = self.START_STATE
+
+        for ele in in_string:
+            check_state = self.run_state_transition(ele)
+            # Check if new state is not REJECT
+            if check_state == 'REJECT':
+                return False
+        return self.check_if_accept()
---
+++ TextColor.py
@@ -0,0 +1,10 @@
+class TextColor:
+    NORMAL = '\033[0m'
+    HEADER = '\033[1;34m'
+    NOTE = '\033[3;4m'  # italics, underline
+    HIGHLIGHT = '\033[1;36m'  # bold, cyan
+    ACCEPT = '\033[1;32m'  # bold, green
+    REJECT = '\033[1;93m'  # bold, yellow
+    BOLD = '\033[1;4m'  # bold, underline
+    ERROR = '\033[91m'  # bold, red
+
